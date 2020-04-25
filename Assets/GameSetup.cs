b54e3b99cc573a7822802a0bb1accd7d1e47c367
Jack <gungravefire@yahoo.com>
2020-04-25 16:51:10 -0500

    Paddles, borders and gamecontrol created

---
+++ Assets/GameSetup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour {
+    public Camera mainCam;
+    public BoxCollider2D topWall;
+    public BoxCollider2D bottomWall;
+    public BoxCollider2D leftWall;
+    public BoxCollider2D rightWall;
+
+    public Transform player01;
+    public Transform player02;
+
+    // Use this for initialization
+    void Start () {
+        topWall.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x * 2f, 1f);
+        topWall.offset = new Vector2(0f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + 0.5f);
+
+        bottomWall.size = topWall.size;
+        bottomWall.offset = -topWall.offset;
+
+        leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y * 2f);
+        leftWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+
+        rightWall.size = leftWall.size;
+        rightWall.offset = -leftWall.offset;
+
+        player01.position = new Vector3(mainCam.ScreenToWorldPoint(new Vector3(75f, 0f, 0f)).x, 0f, 0f);
+        player02.position = new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width - 75f, 0f, 0f)).x, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+        //Move the players to a fixed distance from the edges of the screen:
+
+    }
+}
